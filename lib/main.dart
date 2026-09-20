b8741793e61731bb9573e3be90e3d2cb8a638939
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-20 14:57:49 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'pages/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const BreezyApp());
 }
 
@@ -13,15 +21,13 @@ class BreezyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Breezy',
-
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 4, 18, 30),
+          seedColor: const Color(0xFF42A5F5),
         ),
         scaffoldBackgroundColor: const Color(0xFFF5FAFF),
       ),
-
       home: const SplashPage(),
     );
   }
